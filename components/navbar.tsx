9bcaa98c739f5a9edb1140a137e897c239e0c82f
Luciano Arias <lucianoarias1997@gmail.com>
2024-03-10 15:43:34 -0300

    initial commit

---
+++ components/navbar.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { ThemeDropdown } from '@/app/themeDropdown/themeDropdown';
+import { github } from '@/app/utils/icons';
+import { Button } from '@/components/ui/button';
+import { useRouter } from 'next/navigation';
+import SearchDialog from '@/components/search-dialog';
+
+export default function Navbar() {
+  const router = useRouter();
+  return (
+    <div className="w-full py-4 flex items-center justify-between">
+      <div className="left"></div>
+      <div className="search-container flex shrink-0 w-full gap-2 sm:w-fit">
+        <SearchDialog />
+        <div className="btn-group flex items-center gap-2">
+          <ThemeDropdown />
+          <Button
+            onClick={() => {
+              router.push('https//github.com');
+            }}
+            className="source-code flex items-center gap-2"
+          >
+            {github} Source code
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
